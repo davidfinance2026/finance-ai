c0ed5053e5728144424a1cd856b1a86f592eb991
davidfinance2026 <davidferreir779@gmail.com>
2026-03-01 04:42:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -960,8 +960,11 @@ def wa_webhook():
         return "ok", 200
     finally:
         db.close()
-
+# Criar tabelas automaticamente
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
