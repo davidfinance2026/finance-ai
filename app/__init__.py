a1a14d1d3e580c3d0e555685126b4385fa6ec641
davidfinance2026 <davidferreir779@gmail.com>
2026-03-12 00:13:10 -0300

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object("config")
+
+    db.init_app(app)
+    CORS(app)
+
+    from .routes.auth import auth_bp
+    from .routes.account import account_bp
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(account_bp)
+
+    return app
